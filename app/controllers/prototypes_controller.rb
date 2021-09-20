e06014223cf4c3c725bc7c11019d38576c16ab6c
kinta324 <89384762+kinta324@users.noreply.github.com>
2021-09-20 18:53:09 +0900

    2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,6 +27,7 @@ class PrototypesController < ApplicationController
   end
 
   def edit
+    @prototype = Prototype.find(params[:id])
   end
 
   def update
@@ -48,7 +49,7 @@ class PrototypesController < ApplicationController
   private
 
   def prototype_params
-    params.permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
   def set_prototype
